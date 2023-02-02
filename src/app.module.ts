ef763912c7e136ea04ab406b6e89c870942982c6
kipkoech <nickey968@gmail.com>
2023-02-02 20:10:28 +0300

    chore :added mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import { AuthModule } from './Auth/auth.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
