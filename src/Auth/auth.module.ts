3091ddedcd93b0cba643066836fe9b97d218036f
kipkoech <nickey968@gmail.com>
2023-02-02 20:10:53 +0300

    chore :added mongoose module

--- src/Auth/auth.module.ts
+++ src/Auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
+  imports: [MongooseModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
