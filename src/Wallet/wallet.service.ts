9e48af752c87352dc0b71b0003e26c48e68a3e00
kipkoech <nickey968@gmail.com>
2023-02-03 10:12:21 +0300

    user wallet reference

--- src/Wallet/wallet.service.ts
+++ src/Wallet/wallet.service.ts
@@ -1,1 +1,36 @@
-export class WalletService {}
+import { Wallet } from './wallet.model';
+import { Injectable, NotFoundException, Param } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Error, Model } from 'mongoose';
+import { User } from 'src/Auth/auth.model';
+
+@Injectable()
+export class WalletService {
+  constructor(
+    @InjectModel('Wallet') private walletModel: Model<Wallet>,
+    @InjectModel('User') private userModel: Model<User>,
+  ) {}
+  async createWallet(
+    userEmail: string,
+    address: string,
+    balance: number,
+    accountName: string,
+  ) {
+    const user: string = await this.userModel.findOne({ email: userEmail });
+    const wallet = new this.walletModel({
+      user: user,
+      address: address,
+      accountName: accountName,
+      balance: balance,
+    });
+    await wallet.save();
+    return { wallet };
+  }
+  async getWallets() {
+    const wallets = await this.walletModel.find({}).exec();
+    if (wallets.length === 0) {
+      throw new NotFoundException('Wallets not found!');
+    }
+    return { wallets };
+  }
+}
