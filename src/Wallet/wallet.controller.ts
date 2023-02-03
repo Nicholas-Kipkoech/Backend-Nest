0c438357ec4648d2c5800771ce1081d0c95923e1
kipkoech <nickey968@gmail.com>
2023-02-03 10:11:25 +0300

    created user reference to every wallet

--- src/Wallet/wallet.controller.ts
+++ src/Wallet/wallet.controller.ts
@@ -1,1 +1,26 @@
-export class WalletController {}
+import { WalletService } from './wallet.service';
+import { Body, Controller, Post, Param, Get } from '@nestjs/common';
+@Controller('wallets')
+export class WalletController {
+  constructor(private readonly walletService: WalletService) {}
+  @Post(':email')
+  createWallet(
+    @Param('email') user: string,
+    @Body('address') address: string,
+    @Body('balance') balance: number,
+    @Body('accountName') accountName: string,
+  ): Promise<any> {
+    const createWallet = this.walletService.createWallet(
+      user,
+      address,
+      balance,
+      accountName,
+    );
+    return createWallet;
+  }
+  @Get()
+  getAllWallets() {
+    const wallets = this.walletService.getWallets();
+    return wallets;
+  }
+}
