729fda8ae1e069672d92bfbcaf183c6707fb7fe0
kipkoech <nickey968@gmail.com>
2023-02-04 20:27:58 +0300

    add new service for getting user and its wallets

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller('user')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post()
+  @Post('create')
+  //create user
   createUser(
     @Body('username') username: string,
     @Body('email') userEmail: string,
@@ -17,9 +18,15 @@ export class AuthController {
     );
     return createdUser;
   }
-  @Get('users')
+  //get all users
+  @Get('fetch/users')
   async getAllUsers() {
     const users = await this.authService.getUsers();
     return { users: users.users };
+  }
+  @Get('fetch/:userID')
+  async getUserAndWallet(@Param('userID') userID: string) {
+    const fetchedUser = await this.authService.getOneUserAndWallets(userID);
+    return { fetchedUser };
   }
 }
