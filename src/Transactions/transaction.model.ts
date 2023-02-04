124d5d642951eacb9cc9facc87c905428e032440
kipkoech <nickey968@gmail.com>
2023-02-04 11:54:57 +0300

    chore:working on schema

--- src/Transactions/transaction.model.ts
+++ src/Transactions/transaction.model.ts
@@ -1,3 +1,18 @@
 import * as mongoose from 'mongoose';
 
-export const transactionSchema = new mongoose.Schema({});
+export const transactionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    wallet: { type: mongoose.Schema.Types.ObjectId, ref: 'Wallet' },
+    desc: { type: String, required: true },
+    amount: { type: Number, required: true },
+  },
+  { timestamps: true },
+);
+
+export interface Transaction {
+  user?: any;
+  wallet?: any;
+  desc: string;
+  amount: number;
+}
