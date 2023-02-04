09ba211fa4d5f2db4e184c53620fdae508134ac5
kipkoech <nickey968@gmail.com>
2023-02-04 11:54:24 +0300

    chore:worked on transaction controller

--- src/Transactions/transaction.controller.ts
+++ src/Transactions/transaction.controller.ts
@@ -1,1 +1,21 @@
-export class TransactionController {}
+import { TransactionService } from './transaction.service';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+@Controller('transaction')
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+  @Post(':user/:wallet')
+  createTransaction(
+    @Param('user') user: string,
+    @Param('wallet') wallet: string,
+    @Body('desc') desc: string,
+    @Body('amount') amount: number,
+  ): Promise<any> {
+    const newTransaction = this.transactionService.createTransaction(
+      user,
+      wallet,
+      desc,
+      amount,
+    );
+    return newTransaction;
+  }
+}
