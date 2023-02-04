95fdf68d4621066463772b65829525b58dedf489
kipkoech <nickey968@gmail.com>
2023-02-04 11:55:34 +0300

    chore:working on transaction module adding schema

--- src/Transactions/transaction.module.ts
+++ src/Transactions/transaction.module.ts
@@ -1,1 +1,19 @@
+import { TransactionService } from './transaction.service';
+import { TransactionController } from './transaction.controller';
+import { transactionSchema } from './transaction.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { walletSchema } from 'src/Wallet/wallet.model';
+import { UserSchema } from 'src/Auth/auth.model';
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Transaction', schema: transactionSchema },
+    ]),
+    MongooseModule.forFeature([{ name: 'Wallet', schema: walletSchema }]),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
+  controllers: [TransactionController],
+  providers: [TransactionService],
+})
 export class TransactionModule {}
